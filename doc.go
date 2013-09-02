e31b3e723f047958260695b921c2cc8172f8bc5e
lcooper <lcooper@86b54a1d-3613-465c-8040-c776a987f4ec>
2013-09-02 13:46:05 +0000

    Add package level docs.

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Package sling provides a connection limited HTTP client which ensures that 
+// HTTP keep-alive is used whenever possible.
+//
+// Additionally, it provides a fluent DSL for constructing HTTP requests with
+// JSON bodies and processing JSON responses, as well as flexible handling
+// of HTTP status to error translation.
+package sling
